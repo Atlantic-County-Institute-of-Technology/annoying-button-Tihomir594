c7572614aa6884902b4ba03f30db5fd20b2aa6e6
Tihomir594 <valc0465@student.acitech.org>
2025-03-03 09:12:36 -0500

    Made the button say text.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const button1 = document.getElementById("button1");
+let text = document.getElementById("Text");
+let clicks = 0;
 
 button1.addEventListener("click",yell_at_user);
 function yell_at_user() {
-    alert("STOP PRESSING THIS BUTTON!")
+    clicks++;
+    if(clicks == 1){
+        text.innerHTML = "Hey! Stop that!";
+    }
+    else if(clicks == 3){
+        text.innerHTML = "Im warning you!";
+    }
+    else if(clicks == 7){
+        text.innerHTML = "Now try to catch me!";
+    }
 }
